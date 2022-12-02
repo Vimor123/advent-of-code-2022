79c978c51d272c2ca2dc89cdc3da573cbef63d75
Vimor123 <vmladar1@gmail.com>
2022-12-02 07:29:39 +0100

    day 2

---
+++ day2/task2.py
@@ -0,0 +1,36 @@
+inputFile = open("input.txt", "r")
+
+games = []
+
+for line in inputFile:
+    games.append(line.split())
+
+total = 0
+
+for i in range(len(games)):
+    
+    if games[i][0] == "A":
+        if games[i][1] == "X":
+            total += 3
+        elif games[i][1] == "Y":
+            total += 4
+        elif games[i][1] == "Z":
+            total += 8
+
+    elif games[i][0] == "B":
+        if games[i][1] == "X":
+            total += 1
+        elif games[i][1] == "Y":
+            total += 5
+        elif games[i][1] == "Z":
+            total += 9
+
+    elif games[i][0] == "C":
+        if games[i][1] == "X":
+            total += 2
+        elif games[i][1] == "Y":
+            total += 6
+        elif games[i][1] == "Z":
+            total += 7
+
+print(total)
